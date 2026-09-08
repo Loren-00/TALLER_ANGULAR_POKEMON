923e90a8055e8376ff0359cf0bbafb3b3aa8d5ad
Loren-00 <lorendiaz3412@gmail.com>
2026-09-08 17:12:57 +0000

    creacion de paginas

--- MiPrimerAngular/src/app/app.component.ts
+++ MiPrimerAngular/src/app/app.component.ts
@@ -1,43 +1,13 @@
-import { Component, inject, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { PokemonService, Pokemon } from './pokemon.service';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './app.component.html', 
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  private pokemonService = inject(PokemonService);
-
-  query = signal('ditto');
-  pokemon = signal<Pokemon | null>(null);
-  loading = signal(false);
-  error = signal<string | null>(null);
-
-  buscar(): void {
-    const name = this.query().trim();
-    if (!name) {
-      this.error.set('Escribe el nombre de un Pokémon.');
-      this.pokemon.set(null);
-      return;
-    }
-
-    this.loading.set(true);
-    this.error.set(null);
-
-    this.pokemonService.getPokemon(name).subscribe({
-      next: (result) => {
-        this.pokemon.set(result);
-        this.loading.set(false);
-      },
-      error: () => {
-        this.error.set(`No se encontró ningún Pokémon llamado "${name}".`);
-        this.pokemon.set(null);
-        this.loading.set(false);
-      },
-    });
-  }
+  titulo = 'Bienvenidos al Buscador Pokemon con mas Aura de la 3227025 ;D';
 }
--- MiPrimerAngular/src/main.ts
+++ MiPrimerAngular/src/main.ts
@@ -1,7 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes'
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
-}).catch((err) => console.error(err));
+  providers: 
+    [provideRouter(routes)]
+}).catch((error) => console.error('[LeroLeroCandelero] Angular bootstrap failed:', error));
+
